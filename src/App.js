65b635366b59380badeda23baad5c2d29b977015
Mathieu DEJOB <dejob.mathieu@gmail.com>
2022-08-02 18:19:48 +0200

    Add HTML structure and content (except 3d model)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
+// Styles
 import "./assets/styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      {/* HTML page contents */}
+      <div className="container">
+        {/* Main title */}
+        <div className="titleContainer">
+          <h1>
+            besoin d'un
+            <br />
+            <span>site web?</span>
+          </h1>
+        </div>
+
+        {/* Website 3D model */}
+
+        {/* Technologies */}
+        <div className="skills">
+          <ul>
+            <li>HTML / CSS</li>
+            <li>JavaScript</li>
+            <li>REACT</li>
+            <li>Tailwind CSS</li>
+            <li>CMS (Wordpress / Webflow)</li>
+          </ul>
+          <h2>Compétences</h2>
+        </div>
+
+        {/* Contacts */}
+        <div className="contactsContainer">
+          <div className="contactsTitle">
+            <h2>contacts</h2>
+          </div>
+          <div className="contactsBox">
+            <h3>tel</h3>
+            <p>+336 31 66 30 86</p>
+          </div>
+
+          <div className="contactsBox">
+            <h3>mail</h3>
+            <p>dejob.mathieu@gmail.com</p>
+          </div>
+        </div>
+
+        {/* A propos */}
+        <div className="apropos">
+          <ul>
+            <li>Mathieu - 29 ans 🙋‍♂️</li>
+            <li>Développeur web frontend 💻</li>
+            <li>Auto-entrepreneur 🏠👨‍💻</li>
+            <li>Depuis juillet 2021 🗓️</li>
+          </ul>
+          <h2>A propos</h2>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
